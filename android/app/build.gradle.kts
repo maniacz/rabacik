f44514231a8f89c0e4da4c5193c92bd9dce75ca8
Lukasz Kipka <lukasz.kipka@ipsos.com>
2026-03-16 11:03:19 +0100

    Exclude Andoid Profile Installer for development performance reasons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    configurations.all {
+        exclude(group = "androidx.profileinstaller", module = "profileinstaller")
+    }
 }
 
 flutter {
